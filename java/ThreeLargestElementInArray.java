6575c31d4b8b30fd14fadcc706ca1f166bf81c85
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-01-23 22:21:56 +0530

    Create ThreeLargestElementInArray

---
+++ java/ThreeLargestElementInArray.java
@@ -0,0 +1,27 @@
+private class Algorithms { 
+
+	public static void main(String[] args) {
+		int arr[] = {30,-1,4,8,20,1,0}; 
+		int n = arr.length;  
+    int i, first, second, third; 
+
+		if (n < 3) { 
+			  System.out.print("Array Size Less Than 3"); 
+		}else{
+        third = first = second = Integer.MIN_VALUE; 
+        
+		    for (i=0;i<n;i++) {
+			      if (arr[i] > first) { 
+				  	    third = second; 
+				        second = first; 
+				        first = arr[i]; 
+			      } else if (arr[i] > second) { 
+				        third = second; 
+				        second = arr[i]; 
+			      } else if (arr[i] > third) 
+				        third = arr[i]; 
+		        }        
+		    System.out.println("Three largest elements are " + first + second + third); 
+    }	
+	} 
+} 
