977be9c6900200b1dcb66ed975a37516c2e1b469
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-01-23 22:57:08 +0530

    Create ThreeLargestElementInArray

---
+++ python/ThreeLargestElementInArray.py
@@ -0,0 +1,32 @@
+import sys 
+ 
+def print3largest(arr, n): 
+    
+    if (n < 3): 
+      
+        print("Array Size Less Than 3") 
+        return
+      
+    third = first = second = -sys.maxsize 
+      
+    for i in range(0, n): 
+      
+        if (arr[i] > first): 
+          
+            third = second 
+            second = first 
+            first = arr[i] 
+            
+        elif (arr[i] > second): 
+          
+            third = second 
+            second = arr[i] 
+          
+        elif (arr[i] > third): 
+            third = arr[i] 
+      
+    print("Three largest elements are",first, second, third) 
+    
+arr = [1,20,-4,10,90,21,6] 
+n = len(arr) 
+print3largest(arr, n) 
