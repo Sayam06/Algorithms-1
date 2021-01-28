8f5103018446762fa5d6980a35fb1d4a5588af61
Ishaan <ishaangupta999@gmail.com>
2021-01-28 20:12:46 +0530

    Factorial added

---
+++ c++/Factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std; 
+  
+int factorial(int num) { 
+    if(num == 0) 
+          return 1; 
+    int i = num, fact = 1; 
+    while (num / i != num) { 
+        fact = fact * i; 
+        i--; 
+    } 
+    return fact; 
+} 
+  
+int main() { 
+    int num = 5;
+    cin>>num; 
+    cout<<"Factorial of "<<num<<" is "<<factorial(num)<<endl; 
+    return 0; 
+} 
