7ada5c891808c0406d9b8e806693edb1dc9926e1
Ishaan <ishaangupta999@gmail.com>
2021-01-28 20:22:34 +0530

    Factorial Added

---
+++ python/Factorial.py
@@ -0,0 +1,15 @@
+def factorial(num): 
+    if(num == 0): 
+       return 1
+    i = num 
+    fact = 1
+      
+    while(num / i != num): 
+        fact = fact * i 
+        i -= 1
+          
+    return fact 
+  
+num = 5
+print("Factorial of", num, "is",factorial(num)) 
+   
