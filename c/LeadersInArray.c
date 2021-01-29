bf14551147c383c272a1cc3ea0ec0f3e96596963
Ishaan <ishaangupta999@gmail.com>
2021-01-29 15:57:02 +0530

    LeadersAdded

---
+++ c/LeadersInArray.c
@@ -0,0 +1,19 @@
+void Leaders(int arr[], int n){
+    int max =  arr[n-1];
+    int i;
+ 
+    printf("%d ", max);
+      
+    for(i = n-2; i >= 0; i--){
+        if(max < arr[i]){
+           printf("%d ", arr[i]);
+           max = arr[i];
+        }
+    }   
+}
+ 
+int main(){
+  int arr[] = {21, 20, 50, 2, 18, 11 , 3, 6, 0};
+  Leaders(arr, 9);
+  return 0; 
+}  
