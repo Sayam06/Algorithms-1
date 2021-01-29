c6418133c0ad5d3ee62ecd8a8864402f4dc030a7
Ishaan <ishaangupta999@gmail.com>
2021-01-29 15:48:42 +0530

    Leaders Added

---
+++ c++/LeadersInArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std; 
+  
+void leaders(int arr[], int size){
+    int maxright =  arr[size-1]; 
+  
+    cout<<maxright; 
+      
+    for (int i = size-2; i >= 0; i--) { 
+        if (maxright <= arr[i]) {            
+            maxright = arr[i]; 
+            cout << maxright; 
+        } 
+    }     
+} 
+  
+int main() { 
+    int arr[] = {21, 20, 50, 2, 18, 11 , 3, 6, 0}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    leaders(arr, n); 
+    return 0; 
+}
