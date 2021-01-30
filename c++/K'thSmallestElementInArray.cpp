77f718dc5cf8fdf59e23fde4f86f981cb84cdd31
Ishaan <ishaangupta999@gmail.com>
2021-01-30 15:20:54 +0530

    K'th Smallest Added

---
+++ c++/K'thSmallestElementInArray.cpp
@@ -0,0 +1,18 @@
+#include <algorithm> 
+#include <iostream> 
+using namespace std; 
+   
+int smallest(int arr[], int n, int k) { 
+    
+    sort(arr, arr + n); 
+  
+    return arr[k - 1]; 
+} 
+
+int main(){
+    int a[5] = {10,5,7,20,13};
+    int n = 5,k=0;
+    cin>>k;
+    cout<<smallest(a, n, k);
+    return 0;
+} 
