a5f7c465108a392bda75f1c9ab87197116a6b005
Ishaan <ishaangupta999@gmail.com>
2021-01-30 18:24:23 +0530

    Missing Number.cpp Added

--- c++/MissingNumber.cpp
+++ c++/MissingNumber.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
 using namespace std;
- 
-int missing(int a[], int n) { 
-    int i, sum=1; 
-     
-    for (i=2;i<=(n+1);i++){
-        sum+=i;
-        sum -= a[i-2];
-    }
-    return sum; 
-} 
 
 void sort(int arr[], int n) { 
 	int i=0, j=0;  
@@ -23,6 +13,18 @@ void sort(int arr[], int n) {
         }
     }
 } 
+ 
+int missing(int a[], int n) { 
+    int i, sum=1; 
+     
+    for (i=2;i<=(n+1);i++){
+        sum+=i;
+        sum -= a[i-2];
+    }
+    return sum; 
+} 
+
+
  
 int main() {
     int arr[] = {1, 2, 3, 4, 5 ,7};
