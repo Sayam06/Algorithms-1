cc667c6d7102ed9ee5d797ea495911c9f6226dd3
Ishaan <ishaangupta999@gmail.com>
2021-01-30 18:37:38 +0530

    SortAddedIn Missing Number.c

--- c/MissingNumber.c
+++ c/MissingNumber.c
@@ -10,8 +10,22 @@ int missing(int a[], int n) {
     return sum; 
 } 
  
+void sort(int arr[], int n) { 
+    int i=0, j=0;  
+    for (i = 0; i < n-1; i++){  
+        for (j = 0; j < n-i-1; j++){  
+            if (arr[j] > arr[j+1]){ 
+                int temp = arr[j];  
+                arr[j] = arr[j+1];  
+                arr[j+1] = temp; 
+            }
+        }
+    }
+} 
+
 int main() {
     int arr[] = {1, 2, 3, 5, 6};
+    sort(arr,n);
     cout<<missing(arr,sizeof(arr)/sizeof(arr[0]));
     return 0;
 }
