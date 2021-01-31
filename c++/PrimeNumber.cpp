4ca66f7cc188e2e888eae4188531584df3d79748
Ishaan <ishaangupta999@gmail.com>
2021-01-31 23:23:15 +0530

    PrimeNumber.cpp Updated

--- c++/PrimeNumber.cpp
+++ c++/PrimeNumber.cpp
@@ -8,6 +8,11 @@ int main(){
     cin>>n;
     bool flag = true;
 
+    if(n <= 1){
+        cout<<"Wrong Input";
+        return 0;
+    }
+
     for(int i=2;i<=sqrt(n);i++){
         if(n%i == 0){
             cout<<"Non Prime";
@@ -17,7 +22,7 @@ int main(){
     }
 
     cout<<((flag) ? "Prime Number" : "");
-    
+
     return 0;
 }
    
