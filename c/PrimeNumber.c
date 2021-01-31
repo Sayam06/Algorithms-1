201c6d6aafa7ebda482adb26d8cf5c8b78cfbbcb
Ishaan <ishaangupta999@gmail.com>
2021-01-31 23:19:09 +0530

    Prime.c Added

---
+++ c/PrimeNumber.c
@@ -0,0 +1,25 @@
+#include<iostream.h>
+#include<cmath.h>
+
+int main(){
+    int n = 0;
+
+    scanf("%d", &n);
+
+    bool flag = true;
+
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i == 0){
+            printf( "Non Prime");
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag){
+            printf( "Prime");
+    }
+    
+    return 0;
+}
+   
