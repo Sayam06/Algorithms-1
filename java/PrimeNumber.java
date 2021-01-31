d80629aa5202e2c09a4bb4657a11d60a2bd8572a
Ishaan <ishaangupta999@gmail.com>
2021-01-31 23:29:28 +0530

    PrimeNumber.java Added

--- java/PrimeNumber.java
+++ java/PrimeNumber.java
@@ -1,17 +1,11 @@
 public class Algorithms {
  
-    static boolean isPrime(int n){
+    static boolean prime(int n){
  
         if (n <= 1)
             return false;
  
-        else if (n == 2)
-            return true;
- 
-        else if (n % 2 == 0)
-            return false;
- 
-        for (int i = 3; i <= Math.sqrt(n); i += 2) 
+        for (int i = 2; i <= Math.sqrt(n); i++) 
         {
             if (n % i == 0)
                 return false;
@@ -20,7 +14,7 @@ public class Algorithms {
     }
  
     public static void main(String[] args){
-        if (isPrime(19))
+        if (prime(19))
             System.out.println("true");
  
         else
