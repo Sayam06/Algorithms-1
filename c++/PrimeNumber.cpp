da935d51d815a6322e0ab1fb4e6469c1b157b709
Ishaan <ishaangupta999@gmail.com>
2021-01-31 23:14:50 +0530

    Prime.cpp Added

--- c++/PrimeNumber.cpp
+++ c++/PrimeNumber.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main(){
     int n = 0;
+
     cin>>n;
     bool flag = true;
+
     for(int i=2;i<=sqrt(n);i++){
         if(n%i == 0){
             cout<<"Non Prime";
@@ -14,9 +16,8 @@ int main(){
         }
     }
 
-    if(flag){
-        cout<<"Prime Number";
-    }
+    cout<<((flag) ? "Prime Number" : "");
+    
     return 0;
 }
    
