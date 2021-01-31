44c7e68e5c095a6b59fd6125e09b50dbd0e0c116
Ishaan <ishaangupta999@gmail.com>
2021-01-31 23:24:01 +0530

    Prime.c Updated

--- c/PrimeNumber.c
+++ c/PrimeNumber.c
@@ -8,6 +8,11 @@ int main(){
 
     bool flag = true;
 
+    if(n <= 1){
+        printf("Wrong Input");
+        return 0;
+    }
+
     for(int i=2;i<=sqrt(n);i++){
         if(n%i == 0){
             printf( "Non Prime");
