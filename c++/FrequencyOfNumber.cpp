8b34ef50d5ec27802da6050888b131cce2d1db1b
Ishaan <ishaangupta999@gmail.com>
2021-02-01 18:08:35 +0530

    Added FrequencyOfANumber.cpp

--- c++/FrequencyOfNumber.cpp
+++ c++/FrequencyOfNumber.cpp
@@ -3,16 +3,20 @@ using namespace std;
   
 int find(int a[], int n, int x){ 
     int ctr = 0; 
-    for (int i=0; i < n; i++) 
+
+    for (int i=0;i < n;i++) 
        if (a[i] == x)  
           ctr++; 
+
     return ctr; 
 } 
    
 int main() { 
-    int arr[] = {0, 5, 5, 5, 4}; 
-    int x = 5; 
+    int arr[] = {2002,1,12,12,12,22,1,2001,10}; 
+    int x = 12; 
     int n = sizeof(arr)/sizeof(arr[0]); 
-    cout<<find(arr, n, x); 
+
+    cout<<find(arr, n, x);
+
     return 0; 
 }  
