8b6cfb87288349a6667f7e53dfd2411d568ea51a
Ishaan <ishaangupta999@gmail.com>
2021-02-01 18:13:24 +0530

    Frequency.c Added

---
+++ c/FrequencyOfNumber.c
@@ -0,0 +1,23 @@
+#include<stdio.h>  
+  
+int find(int a[], int n, int x){ 
+    int ctr = 0,i=0; 
+
+    for (i=0;i < n;i++) 
+       if (a[i] == x)  
+          ctr++; 
+
+    return ctr; 
+} 
+   
+int main() { 
+    int arr[] = {2002,1,12,12,12,22,1,2001,10}; 
+    int x = 12; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+
+    int res = find(arr, n, x);
+
+    printf("%d",res);
+
+    return 0; 
+}  
