8a4e892d3ed7f2ca27ef2c933b94731372f622a4
Ishaan <ishaangupta999@gmail.com>
2021-02-01 18:18:31 +0530

    FrequencyOfAnumber.java Added

--- java/FrequencyOfNumber.java
+++ java/FrequencyOfNumber.java
@@ -1,21 +1,19 @@
-
-  
 public class Algorithms { 
       
-    static int frequency(int a[],int n, int x){ 
-        int count = 0; 
+    static int find(int a[],int n, int x){ 
+        int ctr = 0; 
         for (int i=0; i < n; i++) 
         if (a[i] == x)  
-            count++; 
-        return count; 
+            ctr++; 
+        return ctr; 
     } 
       
     public static void main (String[] args) { 
           
-        int a[] = {2002,1,12,12,12,22,1,2001,10}; 
+        int arr[] = {2002,1,12,12,12,22,1,2001,10}; 
         int x = 2002; 
         int n = a.length; 
           
-        System.out.println(frequency(a, n, x)); 
+        System.out.println(find(arr, n, x)); 
     } 
 } 
