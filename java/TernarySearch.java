168d6ce7f26958ec3cb54671de67cedb7a2d9a35
Ishaan <ishaangupta999@gmail.com>
2021-02-02 16:06:45 +0530

    TernarySearch.java Added

--- java/TernarySearch.java
+++ java/TernarySearch.java
@@ -1,18 +1,14 @@
 public class Algorithms {
- 
+    
     static int ternary(int l, int r, int x, int arr[]){
         if (r >= l) {
  
             int mid1 = l + (r - l) / 3;
             int mid2 = r - (r - l) / 3;
  
-            if (arr[mid1] == x) {
-                return mid1;
+            if (arr[mid1] == x || arr[mid2] == x) {
+                return x;
             }
-            
-            if (arr[mid2] == x) {
-                return mid2;
-            } 
  
             if (x < arr[mid1]) {
                 return ternary(l, mid1 - 1, x, arr);
@@ -27,12 +23,17 @@ public class Algorithms {
  
         return -1;
     }
- 
-    public static void main(String args[]){
- 
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};       
-        int l=0, r=arr.length, p, x=12;
 
-        System.out.println("Index of " + x + " is " + ternary(l, r, x, arr)); 
+    public static void main(String args[]){
+         
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};       
+        int l=0, r=arr.length-1, x=12;
+        int res = ternary(l, r, x, arr);
+        
+        if(res != -1) 
+            System.out.println(x + " is present at " +(res-1));
+        else 
+            System.out.println(x + " is not present");       
+         
     }
 }
