089d8067e4260f47c92b0a911253b7a9717ba7ea
Ishaan <ishaangupta999@gmail.com>
2021-02-03 19:13:26 +0530

    K'th Largest Element Added

--- c++/K'thLargestElementInArray.cpp
+++ c++/K'thLargestElementInArray.cpp
@@ -6,7 +6,6 @@ int largest(int arr[], int n, int k){
     for (int i = 0; i < n-1; i++){ 
         for (int j = 0; j < n-i-1; j++){ 
             if (arr[j] > arr[j+1]){
-                // swaping by taking a temp variable
                 int temp = arr[j]; 
                 arr[j] = arr[j+1]; 
                 arr[j+1] = temp; 
@@ -14,7 +13,7 @@ int largest(int arr[], int n, int k){
         }
     }
 
-    //return arr[k-1];
+    return arr[k-1];
 }
  
 int main(){
