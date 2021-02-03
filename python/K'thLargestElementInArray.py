d1eb75fda7c3a941b39a321be4411dfa8af6615f
Ishaan <ishaangupta999@gmail.com>
2021-02-03 19:34:23 +0530

    K'th Largest Element Updated

--- python/K'thLargestElementInArray.py
+++ python/K'thLargestElementInArray.py
@@ -1,10 +1,11 @@
-def largest(arr, n, k): 
+def largest(arr, n, k):
 
 	arr.sort(reverse = True) 
+
 	return arr[k-1] 
  
 arr = [1,2002,12,6,9,30,10,30] 
 n = len(arr) 
-k = 3
+k = 5
 print(largest(arr, n, k)) 
 
