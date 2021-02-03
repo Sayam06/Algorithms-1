2318bed7cb3b2dd9333aedaacd168ee2b6bc7ddd
Ishaan <ishaangupta999@gmail.com>
2021-02-03 19:29:08 +0530

    Updated

--- c/K'thLargestElementInArray.c
+++ c/K'thLargestElementInArray.c
@@ -4,7 +4,7 @@ int largest(int arr[], int n, int k){
     
     for (int i = 0; i < n-1; i++){ 
         for (int j = 0; j < n-i-1; j++){ 
-            if (arr[j] > arr[j+1]){
+            if (arr[j] < arr[j+1]){
                 int temp = arr[j]; 
                 arr[j] = arr[j+1]; 
                 arr[j+1] = temp; 
@@ -16,7 +16,7 @@ int largest(int arr[], int n, int k){
 }
  
 int main(){
-    int arr[] = {1,2002,12,6,9,30,10,30};
+    int arr[] = {1,2002,12,6,9,30,10,20};
     int n = 9;
     int k = 3;
     printf(" %d",largest(arr, n, k));
