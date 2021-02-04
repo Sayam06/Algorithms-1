01949edcbebf1a57c98dd5b91f40c144b0bbccfa
Ishaan <ishaangupta999@gmail.com>
2021-02-04 14:17:46 +0530

    PascalTriangle Added

---
+++ c++/PascalTriangle.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int fun(int n){
+	int ctr=1;
+
+	for(int i=2;i<=n;i++){
+		ctr*=i;
+	}
+
+	return ctr;
+}
+
+int main(){
+
+	int n=0;
+	cin>>n;
+
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			cout<<(fun(i)/fun(j)+fun(i-j));
+		}
+	}
+
+	return 0;
+}
