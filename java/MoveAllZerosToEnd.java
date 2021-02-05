2260890055cf92438b7743bc596de8cf379d6ab2
Ishaan <ishaangupta999@gmail.com>
2021-02-05 23:16:59 +0530

    ZeroesToEnd.java Added

--- java/MoveAllZerosToEnd.java
+++ java/MoveAllZerosToEnd.java
@@ -4,7 +4,7 @@ class PushZero {
 	static void func(int arr[], int n) { 
 		int ctr = 0; 
  
-		for (int i = 0; i < n; i++) 
+		for (int i = 0;i < n;i++) 
 			if (arr[i] != 0) 
 				arr[ctr++] = arr[i]; 								
  
@@ -13,9 +13,9 @@ class PushZero {
 	} 
 
 	public static void main (String[] args) { 
-		int arr[] = {1, 2, 3, 0, 0, 0, 10, 9, 8, 0, 5, 0}; 
-		int n = arr.length; 
-		func(arr, n); 
-		
+		int arr[] = {1, 2, 3, 0, 0, 0, 10, 9, 8, 0, 5, 0};		
+		func(arr, arr.length); 
+		for (int i=0; i<arr.length; i++) 
+			System.out.print(arr[i]); 
 	} 
 } 
