2cad0fff6339c6e9982670ed7ad8051c84491c9f
Ishaan <ishaangupta999@gmail.com>
2021-02-05 23:26:11 +0530

    ZeroesToEnd.c Added

--- c/MoveAllZerosToEnd.c
+++ c/MoveAllZerosToEnd.c
@@ -13,11 +13,11 @@ void func(int arr[], int n) {
 
 int main() { 
 	int arr[] = {1, 2, 3, 0, 0, 0, 10, 9, 8, 0, 5, 0}; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
+	int n = 12; 
 	func(arr, n); 
 
 	for (int i = 0; i < n; i++) 
-		cout<<arr[i]; 
+		printf("%d",arr[i]); 
 
 	return 0; 
 } 
