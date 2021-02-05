a9a40aa58fbda22904aa54d45f39946f97b001e7
Ishaan <ishaangupta999@gmail.com>
2021-02-05 23:23:19 +0530

    ZeroesToEnd.cpp Added

--- c++/MoveAllZerosToEnd.cpp
+++ c++/MoveAllZerosToEnd.cpp
@@ -1,24 +1,24 @@
 #include <iostream> 
 using namespace std; 
 
-void func(int arr[], int n) 
-{ 
-	int count = 0; 
+void func(int arr[], int n) { 
+	int ctr = 0; 
 
 	for (int i = 0; i < n; i++) 
 		if (arr[i] != 0) 
-			arr[count++] = arr[i]; 
-								
+			arr[ctr++] = arr[i];								
 
-	while (count < n) 
-		arr[count++] = 0; 
+	while (ctr < n) 
+		arr[ctr++] = 0; 
 } 
 
 int main() { 
 	int arr[] = {1, 2, 3, 0, 0, 0, 10, 9, 8, 0, 5, 0}; 
 	int n = sizeof(arr) / sizeof(arr[0]); 
 	func(arr, n); 
+
 	for (int i = 0; i < n; i++) 
-		cout << arr[i] << " "; 
+		cout<<arr[i]; 
+
 	return 0; 
 } 
