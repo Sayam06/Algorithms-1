ea36f70d86c945ed0bf85cd8582c18cc059d9730
Ishaan <ishaangupta999@gmail.com>
2021-02-06 18:42:02 +0530

    MultipleLeftRotations.cpp Added

--- c++/MultipleLeftRotations.cpp
+++ c++/MultipleLeftRotations.cpp
@@ -6,7 +6,7 @@ void preprocess(int arr[], int n, int temp[]) {
 		temp[i] = temp[i + n] = arr[i]; 
 } 
 
-void leftRotate(int arr[], int n, int k, int temp[]) { 
+void rotate(int arr[], int n, int k, int temp[]) { 
 	int start = k % n; 
 
 	for (int i = start; i < start + n; i++) 
@@ -14,20 +14,20 @@ void leftRotate(int arr[], int n, int k, int temp[]) {
 } 
 
 int main(){ 
-	int arr[] = {1, 3, 5, 7, 9}; 
+	int arr[] = {1, 2, 5, 7, 8, 10, 12, 22,2001, 2002}; 
 	int n = sizeof(arr) / sizeof(arr[0]); 
 
 	int temp[2*n]; 
 	preprocess(arr, n, temp); 
 
-	int k = 2; 
-	leftRotate(arr, n, k, temp); 
+	int k = 3; 
+	rotate(arr, n, k, temp); 
 
-	k = 3; 
-	leftRotate(arr, n, k, temp); 
+	k = 5; 
+	rotate(arr, n, k, temp); 
 
-	k = 4; 
-	leftRotate(arr, n, k, temp); 
+	k = 6; 
+	rotate(arr, n, k, temp); 
 
 	return 0; 
 } 
