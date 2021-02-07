52a42ed0098e46c3802940befc34fac189965b0e
Ishaan <ishaangupta999@gmail.com>
2021-02-07 19:46:35 +0530

    RearrangeArray.py Added

--- python/RearrangeArrayAccordingToIndex.py
+++ python/RearrangeArrayAccordingToIndex.py
@@ -1,24 +1,22 @@
-
-def answer(ar, n):
+def answer(arr, n):
 	
 	for i in range(n):
 		for j in range(n):
-			if (ar[j] == i):
-				ar[j], ar[i] = ar[i], ar[j]
+			if (arr[j] == i):
+				arr[j], arr[i] = arr[i], arr[j]
 
 
 	for i in range(n):		
-		if (ar[i] != i):
-			ar[i] = -1
+		if (arr[i] != i):
+			arr[i] = -1
 
 
 	for i in range(n):
-		print(ar[i])
+		print(arr[i])
 
 
-ar = [-8, 1, 3, 7, 10, 12, 2, 2001, 2002, -666]
+arr = [-8, 1, 3, 7, 10, 12, 2, 2001, 2002, -666]
 
-n = len(ar)
+n = len(arr)
 
-
-answer(ar, n);
+answer(arr, n);
