ed2a6b3d500d0be6bcfd40488d9cd4fc22e27104
Ishaan <ishaangupta999@gmail.com>
2021-02-07 19:10:42 +0530

    RearrangeArray.cpp Added

--- c++/RearrangeArrayAccordingToIndex.cpp
+++ c++/RearrangeArrayAccordingToIndex.cpp
@@ -4,8 +4,8 @@ using namespace std;
 void answer(int arr[], int n){
 	int i, j, temp;
 
-	for (i = 0; i < n; i++){
-		for (j = 0; j < n; j++){
+	for (i = 0;i<n;i++){
+		for (j = 0;j<n;j++){
 			if (arr[j] == i) {
 				temp = arr[j];
 				arr[j] = arr[i];
@@ -15,18 +15,18 @@ void answer(int arr[], int n){
 		}
 	}
 
-	for (i = 0; i < n; i++){
+	for (i = 0;i<n;i++){
 		if (arr[i] != i)
 			arr[i] = -1;		
 	}
 
-	for (i = 0; i < n; i++) {
+	for (i = 0;i<n;i++) {
 		cout<<arr[i];
 	}
 }
 
 int main(){
-	int n, arr[] = { -8, 1, 3, 7, 10, 12, 45, 2001, 2002};
+	int n, arr[] = { -8, 1, 3, 7, 10, 12, 2, 2001, 2002, -666};
 	n = sizeof(arr) / sizeof(arr[0]);
 
 	answer(arr, n);
