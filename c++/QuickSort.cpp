bb976e43044959b0e15cfd74a2af81e6617179ec
Ishaan <ishaangupta999@gmail.com>
2021-02-11 19:26:23 +0530

    QuickSort.cpp Added

--- c++/QuickSort.cpp
+++ c++/QuickSort.cpp
@@ -1,11 +1,6 @@
 #include <iostream.h> 
 using namespace std; 
 
-void swap(int* a, int* b) { 
-	int t = *a; 
-	*a = *b; 
-	*b = t; 
-} 
 int partition (int arr[], int l, int h) { 
 	int pivot = arr[h]; 
 	int i = (l - 1); 
@@ -13,26 +8,33 @@ int partition (int arr[], int l, int h) {
 	for (int j = l; j <= h - 1; j++) { 
 		if (arr[j] < pivot)  { 
 			i++; 
-			swap(&arr[i], &arr[j]); 
+            arr[i] = arr[i] + arr[j];
+            arr[j] = arr[i] - arr[j];
+            arr[i] = arr[i] - arr[j];
 		} 
-	} 
-	swap(&arr[i + 1], &arr[h]); 
+	}  
+    
+    arr[i] = arr[i] + arr[j];
+    arr[j] = arr[i] - arr[j];
+    arr[i] = arr[i] - arr[j];
+            
 	return (i + 1); 
 } 
+
 void sort(int arr[], int l, int h)  { 
 	if (l < h)  { 
-		int pi = partition(arr, l, h); 
-		sort(arr, l, pi - 1); 
-		sort(arr, pi + 1, h); 
+		sort(arr, l, partition(arr, l, h) - 1); 
+		sort(arr, partition(arr, l, h) + 1, h); 
 	} 
 } 
+
 int main()  { 
 	int arr[] = {12,22,10,2002,1,20,7}; 
 	int n = sizeof(arr) / sizeof(arr[0]); 
 
 	sort(arr, 0, n - 1); 
 
-	for (int i = 0; i < size; i++) 
+	for (int i = 0;i<size;i++) 
 		cout<<arr[i]; 
 
 	return 0; 
