bec2a38710d6f8d1a7c8d469ce5d7789704a95a7
Ishaan <ishaangupta999@gmail.com>
2021-02-11 19:22:58 +0530

    QuickSort.cpp Added

--- c++/QuickSort.cpp
+++ c++/QuickSort.cpp
@@ -6,24 +6,24 @@ void swap(int* a, int* b) {
 	*a = *b; 
 	*b = t; 
 } 
-int partition (int arr[], int low, int high) { 
-	int pivot = arr[high]; 
-	int i = (low - 1); 
+int partition (int arr[], int l, int h) { 
+	int pivot = arr[h]; 
+	int i = (l - 1); 
 
-	for (int j = low; j <= high - 1; j++) { 
+	for (int j = l; j <= h - 1; j++) { 
 		if (arr[j] < pivot)  { 
 			i++; 
 			swap(&arr[i], &arr[j]); 
 		} 
 	} 
-	swap(&arr[i + 1], &arr[high]); 
+	swap(&arr[i + 1], &arr[h]); 
 	return (i + 1); 
 } 
-void sort(int arr[], int low, int high)  { 
-	if (low < high)  { 
-		int pi = partition(arr, low, high); 
-		sort(arr, low, pi - 1); 
-		sort(arr, pi + 1, high); 
+void sort(int arr[], int l, int h)  { 
+	if (l < h)  { 
+		int pi = partition(arr, l, h); 
+		sort(arr, l, pi - 1); 
+		sort(arr, pi + 1, h); 
 	} 
 } 
 int main()  { 
@@ -34,6 +34,6 @@ int main()  {
 
 	for (int i = 0; i < size; i++) 
 		cout<<arr[i]; 
-        
+
 	return 0; 
 } 
