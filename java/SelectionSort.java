8d23716bedde2ef6634b6c00d647f7cbcbdfd12f
Ishaan <ishaangupta999@gmail.com>
2021-02-12 19:32:34 +0530

    SelectionSort.java Added

--- java/SelectionSort.java
+++ java/SelectionSort.java
@@ -1,8 +1,8 @@
 public class Algorithms  { 
 	static void selection(int arr[],int n)  { 
 
-		for (int i = 0; i < n-1; i++)  { 
-			//int min = i; 
+		for (int i=0;i<n-1;i++)  { 
+			int min = i; 
 			for (int j = i+1; j < n; j++){ 
 				if (arr[j] < arr[min]) 
                     min = j; 
@@ -16,7 +16,7 @@ public class Algorithms  {
 
 	public static void main(String args[]) 
 	{  
-        int arr[] = {64,25,12,22,11}; 
+        int arr[] = {50,12,22,10,2002,1,20,7}; 
         int n = arr.length;
 
         selection(arr,n); 
