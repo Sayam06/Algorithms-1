db8a3e6c42590c06cb4b0e0a1a3474fd8167dd7d
Ishaan <ishaangupta999@gmail.com>
2021-02-13 22:45:12 +0530

    InsertionSort.java Added

--- java/InsertionSort.java
+++ java/InsertionSort.java
@@ -1,24 +1,20 @@
 public class Algorithms { 
 	static void sort(int arr[])  { 
 		int n = arr.length; 
-		for (int i = 1; i < n; ++i) { 
+
+		for (int i=1;i<n;i++) { 
 			int key = arr[i]; 
-			int j = i - 1; 
+			int j = i-1; 
 
 			while (j >= 0 && arr[j] > key) { 
 				arr[j + 1] = arr[j]; 
-				j = j - 1; 
+				j--;
 			} 
 			arr[j + 1] = key; 
 		} 
 	} 
-	static void printArray(int arr[]) { 
-		int n = arr.length; 
-		
-	} 
-	public static void main(String args[]) 
-	{ 
-		int arr[] = { 12, 11, 13, 5, 6 }; 
+	public static void main(String args[])  { 
+		int arr[] = {2001, 12, 1, 2002, 22, 2001, 10}; 
 
 		sort(arr); 
 
