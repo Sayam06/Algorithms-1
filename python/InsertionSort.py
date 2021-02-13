45e68352b2a7b45ceabe1fdd201f4601b5cf7b0f
Ishaan <ishaangupta999@gmail.com>
2021-02-13 23:13:01 +0530

    InsertionSort.py Added

--- python/InsertionSort.py
+++ python/InsertionSort.py
@@ -1,18 +1,18 @@
-def insertionSort(arr): 
+def sort(arr): 
  
 	for i in range(1, len(arr)): 
 
 		key = arr[i] 
 		j = i-1
-		
+
 		while j >= 0 and key < arr[j] : 
 				arr[j + 1] = arr[j] 
 				j -= 1
 		arr[j + 1] = key 
 
 
-arr = [12, 11, 13, 5, 6] 
-insertionSort(arr) 
-for i in range(len(arr)): 
-	print ("% d" % arr[i]) 
+arr = [20 ,2001, 12, 1, 2002, 22, 2001, 10] 
+sort(arr) 
+for x in arr: 
+	print (x) 
 
