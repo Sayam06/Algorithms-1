e1f80881d14e7a3da9b0ad003223c382896998d7
Ishaan <ishaangupta999@gmail.com>
2021-02-14 20:14:34 +0530

    RecursiveInsertionSort.cpp Added

--- c++/RecursiveInsertionSort.cpp
+++ c++/RecursiveInsertionSort.cpp
@@ -1,22 +1,21 @@
 #include <iostream> 
 using namespace std; 
 
-void sort(int arr[], int n) 
-{ 
+void sort(int arr[], int n) { 
+	
 	if (n <= 1) 
 		return; 
 
-	sort( arr, n-1 ); 
+	sort(arr,n-1); 
 
-	int last = arr[n-1]; 
+	int prev = arr[n-1]; 
 	int j = n-2; 
 
-	while (j >= 0 && arr[j] > last) 
-	{ 
+	while (j >= 0 && arr[j] > prev)  { 
 		arr[j+1] = arr[j]; 
 		j--; 
 	} 
-	arr[j+1] = last; 
+	arr[j+1] = prev; 
 } 
 
 int main() { 
