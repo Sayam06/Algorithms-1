f8ebe2ad724139b223324e80403d35b9d9c7a508
Ishaan <ishaangupta999@gmail.com>
2021-02-15 15:56:31 +0530

    SumOfSubArrays.py Added

--- python/SumOfSubArrays.py
+++ python/SumOfSubArrays.py
@@ -1,11 +1,11 @@
 def sum(arr,n):
 
     for i in range(0,n):
-        sum=0
+        res=0
         for j in range(i,n):
-            sum+=arr[j]
+            res+=arr[j]
 
-        print(sum)
+        print(res, end = ' ')
     
 arr[] = [1,2,3,4,5,6]
-
+sum(arr,len(arr))
