fc90776035437ba708bf23c5c97648ae86d7ae8b
Ishaan <ishaangupta999@gmail.com>
2021-02-15 15:42:04 +0530

    SumOfSubArrays.c Added

--- c/SumOfSubArrays.c
+++ c/SumOfSubArrays.c
@@ -1,16 +1,15 @@
-#include<iostream>
-using namespace std;
+#include<stdio.h>
 
 int main(){
     int arr[] = {1,2,3,4,5,6};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = 6;
 
     for(int i=0;i<n;i++){
         int sum =0;
         for(int j=i;j<n;j++){
             sum+=arr[j];
         }
-        
+        print("%d",sum);        
     }
 
     return 0;
