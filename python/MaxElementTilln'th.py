a2d0417520ee8898fb44c108c9e54e0c6e51b3cd
Ishaan <ishaangupta999@gmail.com>
2021-02-16 19:05:07 +0530

    MaxElementTilln'th.py Added

--- python/MaxElementTilln'th.py
+++ python/MaxElementTilln'th.py
@@ -1,2 +1,14 @@
-def max(arr,n):
-    
+import sys 
+
+def max(arr,n,min):
+    for i in range(0,n):
+        if min < arr[i]:
+            min=arr[i]
+            print(min)
+
+        else:
+            print(min)
+
+arr = [1,2,3,4,1,10,22,2002,12,2001]
+max(arr,len(arr),sys.minint)
+            
