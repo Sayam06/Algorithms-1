428b2a607ced223de2d38c2946eb95ceb48206fc
Ishaan <ishaangupta999@gmail.com>
2021-02-16 18:31:17 +0530

    MaxElementTilln'th.cpp Added

--- c++/MaxElementTilln'th.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int arr[] = {1,2,3,4,2001,10,22,12,2002};
-
-    
-    return 0;
-}
---
+++ c++/MaxElementTillnth.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,1,10,22,2002,12,2001};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int min = INT_MIN;
+    for(int i=0;i<n;i++){
+        
+        cout<<endl;
+    }
+    
+    return 0;
+}
