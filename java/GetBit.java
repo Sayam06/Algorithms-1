1c4425a89eabeff4f71cbad18f0ec76722ed78d6
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-02-21 00:20:22 +0530

    Update GetBit.java

--- java/GetBit.java
+++ java/GetBit.java
@@ -2,7 +2,8 @@ import java.io.*;
 
 class Algorithms {
 	static void getb(long n, long i){
-  
+  		
+		System.out.print(((n & (1 << (i - 1))) >> (i - 1)));
 	}
 
 	public static void main(String[] args) {
