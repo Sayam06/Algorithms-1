7de3e4aabed7a2acb6d41b050bde03695ca8aaa5
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-02-22 16:34:24 +0530

    Create FindUniqueNumberInAnArrayWhereAllNumbersArePresentTwice.c

---
+++ c/FindUniqueNumberInAnArrayWhereAllNumbersArePresentTwice.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int ans(int arr[],int n){
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum=sum^arr[i];
+    }
+    return sum;
+}
+
+int main(){
+    int arr[] = {1,2,4,5,2,5,4,1,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printf("%d",anss(arr,n));
+    return 0;
+}
