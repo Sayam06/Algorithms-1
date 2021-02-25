648943ca4f26552845de8b03ee3e7838300dc8d4
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-02-25 23:54:06 +0530

    Create TowerOfHanoi.c

---
+++ c/TowerOfHanoi.c
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void hanoi(int n, char src, char des, char help)  { 
+	if (n == 1)  { 
+		cout << "Move disk 1 from rod " << src <<  " to rod " << des<<endl; 
+		return; 
+	} 
+	hanoi(n - 1, src, help, des); 
+	cout << "Move disk " << n << " from rod " << src << " to rod " << des << endl; 
+	hanoi(n - 1, help, des, src); 
+} 
+int main() 
+{ 
+	int n = 3;
+	hanoi(n, 'A', 'C', 'B'); 
+	return 0; 
+} 
+
