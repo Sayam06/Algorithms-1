f906b778a5ebb988d9327b526bb16abddff941f1
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-02-26 00:04:52 +0530

    Update TowerOfHanoi.py

--- python/TowerOfHanoi.py
+++ python/TowerOfHanoi.py
@@ -1,7 +1,7 @@
 def hanoi(n , src, dest, help):
 	if n == 1:
 		print("Move disk 1 from rod",src,"to rod",dest)
-	return
+		return
 	hanoi(n-1, src, help, dest)
 	print("Move disk",n,"from rod",src,"to rod",dest)
 	hanoi(n-1, help, dest, src)
