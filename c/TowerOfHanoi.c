ca4686ed659354f42adc0b544db488cd4ad53d64
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-02-25 23:56:33 +0530

    Update TowerOfHanoi.c

--- c/TowerOfHanoi.c
+++ c/TowerOfHanoi.c
@@ -1,17 +1,15 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 void hanoi(int n, char src, char des, char help)  { 
-	if (n == 1)  { 
-		cout << "Move disk 1 from rod " << src <<  " to rod " << des<<endl; 
+	if (n == 1)  {  
+		printf("Move disk 1 from rod  %d to rod %d" ,src,des); 
 		return; 
 	} 
 	hanoi(n - 1, src, help, des); 
-	cout << "Move disk " << n << " from rod " << src << " to rod " << des << endl; 
+	printf("Move disk %d from rod %d to rod %d" ,n,src,des); 
 	hanoi(n - 1, help, des, src); 
 } 
-int main() 
-{ 
+int main() { 
 	int n = 3;
 	hanoi(n, 'A', 'C', 'B'); 
 	return 0; 
