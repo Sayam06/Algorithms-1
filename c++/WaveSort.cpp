e01e1fac1761022e3543fecc7b2d6d1bba16315d
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-03-03 15:16:55 +0530

    Update WaveSort.cpp

--- c++/WaveSort.cpp
+++ c++/WaveSort.cpp
@@ -22,8 +22,9 @@ int main() {
 	int arr[] = {22,10,3,12,21,1,2002,2001}; 
 	int n = sizeof(arr)/sizeof(arr[0]); 
 	sort(arr, n); 
-  
 	
+	for (int i=0; i<n; i++) 
+	  cout<<arr[i]; 	
   
 	return 0; 
 } 
