b42be22cf112686a1221c1547f7024ef688654a3
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-03-04 16:50:48 +0530

    Update JosephusProblem.java

--- java/JosephusProblem.java
+++ java/JosephusProblem.java
@@ -2,19 +2,15 @@ import java.io.*;
 
 class Algorithms{
 
-static int jos(int n, int k)
-{
-if (n == 1)
-	return 1;
+static int jos(int n, int k){
+	if (n == 1)
+	   return 1;
 
 	return (jos(n - 1, k) + k-1) % n + 1;
 
 
-public static void main(String[] args)
-{
-int n = 6;
-int k = 2;
-System.out.println(jos(n, k));
-}
+public static void main(String[] args){
+	System.out.println(jos(6, 2));
+    }
 }
 
