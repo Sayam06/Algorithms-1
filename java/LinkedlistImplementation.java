8315e3e9201c5994a3fb46a9abad1c8a75750fc2
Ishaan <ishaangupta999@gmail.com>
2021-03-08 20:26:12 +0530

    Added

---
+++ java/LinkedlistImplementation.java
@@ -0,0 +1,58 @@
+public class LinkedlistImplementation {
+	
+	public class Node{
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data = data;
+			next = null;
+		}
+	}
+	
+	Node head;
+	
+	boolean isEmpty() {
+		return head == null;
+	}
+	
+	void add(int data) {
+		Node toAdd = new Node(data);
+		
+		if(isEmpty()) {
+			head = toAdd;
+			return;
+		}
+		
+		Node temp = head;
+		while(temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = toAdd;
+	}
+	
+	void printAll() {
+        if(isEmpty()){
+            System.out.println("Empty List");
+            return;
+        }
+		Node temp = head;
+		while(temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+
+	public static void main(String[] args) {
+		LinkedlistImplementation ll = new LinkedlistImplementation();
+		
+		ll.add(12);
+		ll.add(1);
+		ll.add(22);
+		ll.add(10);
+		
+		ll.printAll();
+
+	}
+
+}
