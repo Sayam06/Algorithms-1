5cd995e78eb1f21295a10e64930fe28195fe1650
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-03-09 16:07:22 +0530

    Create ReverseLinkedList.java

---
+++ java/ReverseLinkedList.java
@@ -0,0 +1,76 @@
+public class LinkedlistImplementation {
+	
+	public class Node{
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data = data;
+			next = null;
+		}
+	}
+	
+	Node head;
+	
+	boolean isEmpty() {
+		return head == null;
+	}
+	
+	void add(int data) {
+		Node toAdd = new Node(data);
+		
+		if(isEmpty()) {
+			head = toAdd;
+			return;
+		}
+		
+		Node temp = head;
+		while(temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = toAdd;
+	}
+	
+	void printAll() {
+        if(isEmpty()){
+            System.out.println("Empty List");
+            return;
+        }
+		Node temp = head;
+		while(temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+  
+  void reverse(){
+    if(isEmpty()){
+            System.out.println("Empty List");
+            return;
+    }
+    Node temp = head;
+    Node prev = null;
+    Node nxt = null;
+    
+    while(temp != null){
+        nxt = temp.next;
+        temp.next = prev;
+        prev = temp;
+        temp = nxt;
+    }
+    prev = head    
+  }
+    
+	public static void main(String[] args) {
+		LinkedlistImplementation ll = new LinkedlistImplementation();
+		
+		ll.add(12);
+		ll.add(1);
+		ll.add(22);
+		ll.add(10);
+	  ;;.reverse();	
+        ll.printAll();
+       
+	}
+
+}
